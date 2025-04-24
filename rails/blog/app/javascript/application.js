ff6ffe26b8f70be2c44120f76af5511e941b29bf
Paula Sanchez <paula.sanchez1031@gmail.com>
2025-04-24 14:48:24 -0500

    feat: integrate Rails UJS and enhance importmap configuration for improved JavaScript functionality

--- rails/blog/app/javascript/application.js
+++ rails/blog/app/javascript/application.js
@@ -1,5 +1,11 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "@hotwired/stimulus";
 import "@hotwired/stimulus-loading";
+import "@rails/ujs";
 import "./controllers";
+
+import * as Turbo from "@hotwired/turbo-rails";
+import Rails from "@rails/ujs";
+
+Turbo.start();
+Rails.start();
