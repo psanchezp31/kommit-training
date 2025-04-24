fe7b75609e1e0a6b7b230f4003a2e2227e900e94
Paula Sanchez <paula.sanchez1031@gmail.com>
2025-04-24 11:09:53 -0500

    feat: implement CRUD actions for Posts and Users in their respective controllers

--- rails/blog/app/controllers/posts_controller.rb
+++ rails/blog/app/controllers/posts_controller.rb
@@ -2,4 +2,46 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "Post was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: "Post was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_url, notice: "Post was successfully destroyed."
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
