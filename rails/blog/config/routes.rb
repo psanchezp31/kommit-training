94e27a7ff631164517fc42fb77894541869cd0df
Paula Sanchez <paula.sanchez1031@gmail.com>
2025-04-25 10:27:29 -0500

    feat: add user registration, sign in functionality with new user form and update routes

--- rails/blog/config/routes.rb
+++ rails/blog/config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
   root "posts#index"
-
-  resources :users, only: [ :index, :show, :create ]
-
+  resources :users, only: [ :index, :show, :create, :new ]
   resources :posts
 end
