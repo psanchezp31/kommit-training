69bcbf5b723812636702f9855ed3a390c8c63832
Paula Sanchez <paula.sanchez1031@gmail.com>
2025-04-25 14:24:53 -0500

    feat: update test fixtures and add validations for Post and User models

--- rails/blog/test/models/post_test.rb
+++ rails/blog/test/models/post_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+    @post = Post.new(
+      title: "Test Post",
+      content: "This is a test post content",
+      user: @user
+    )
+  end
+
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "title should be present" do
+    @post.title = nil
+    assert_not @post.valid?
+  end
+
+  test "content should be present" do
+    @post.content = nil
+    assert_not @post.valid?
+  end
+
+  test "should belong to user" do
+    @post.user = nil
+    assert_not @post.valid?
+  end
 end
