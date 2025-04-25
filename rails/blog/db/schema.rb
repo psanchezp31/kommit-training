2d149dfa6fe06584252ef6fc4359c195c67e10bf
Paula Sanchez <paula.sanchez1031@gmail.com>
2025-04-25 09:32:05 -0500

    feat: add sessions table and update schema for user authentication

--- rails/blog/db/schema.rb
+++ rails/blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_23_161557) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_25_140733) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -19,6 +19,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_23_161557) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index [ "user_id" ], name: "index_posts_on_user_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index [ "user_id" ], name: "index_sessions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +39,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_23_161557) do
   end
 
   add_foreign_key "posts", "users"
+  add_foreign_key "sessions", "users"
 end
