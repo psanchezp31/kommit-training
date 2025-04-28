eec21feac9b411830c6d3d39f8b58e6f790f6b89
Paula Sanchez <paula.sanchez1031@gmail.com>
2025-04-28 13:35:33 -0500

    test: add validations for Comment and Follow models with comprehensive test cases

--- rails/blog/test/models/comment_test.rb
+++ rails/blog/test/models/comment_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user1 = users(:one)
+    @user2 = users(:two)
+    @post = posts(:one)
+  end
+
+  test "should be valid" do
+    comment = Comment.new(content: "Great post!", user: @user1, post: @post)
+    assert comment.valid?
+  end
+
+  test "should require content" do
+    comment = Comment.new(user: @user1, post: @post)
+    assert_not comment.valid?
+  end
+
+  test "should require a user" do
+    comment = Comment.new(content: "Great post!", post: @post)
+    assert_not comment.valid?
+  end
+
+  test "should require a post" do
+    comment = Comment.new(content: "Great post!", user: @user1)
+    assert_not comment.valid?
+  end
+
+  test "should not allow comments from non-followers" do
+    comment = Comment.new(content: "Great post!", user: @user2, post: @post)
+    assert_not comment.valid?
+  end
 end
--- rails/blog/test/models/follow_test.rb
+++ rails/blog/test/models/follow_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class FollowTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user1 = users(:one)
+    @user2 = users(:two)
+  end
+
+  test "should be valid" do
+    follow = Follow.new(follower: @user1, followed: @user2)
+    assert follow.valid?
+  end
+
+  test "should require a follower" do
+    follow = Follow.new(followed: @user2)
+    assert_not follow.valid?
+  end
+
+  test "should require a followed user" do
+    follow = Follow.new(follower: @user1)
+    assert_not follow.valid?
+  end
+
+  test "should not allow self-following" do
+    follow = Follow.new(follower: @user1, followed: @user1)
+    assert_not follow.valid?
+  end
+
+  test "should not allow duplicate follows" do
+    Follow.create!(follower: @user1, followed: @user2)
+    follow = Follow.new(follower: @user1, followed: @user2)
+    assert_not follow.valid?
+  end
 end
